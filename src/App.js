4a3da1fbc03d5aec848f0b6a10f1e35a15b1c224
galihriski <pratamagalihriski9@gmail.com>
2020-02-13 14:32:22 +0700

    add night sky

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import Animated from './pages/home/home'
-import './App.css';
+import Animated from './pages/home/home';
+import './App.scss';
 const divStyle = {
-  width: '96%',
-  height: '100%',
-  margin: '20px',
-  padding: '10px'
+  body: {
+    width: '100%',
+    height: '100vh',
+    margin: 0,
+    padding: 0,
+    background: 'rgb(9,76,114)',
+    background: '-webkit-radial-gradient(bottom, ellipse cover,  rgb(9,76,114) 0%,rgb(1,41,63) 100%)',
+    background: '-webkit-radial-gradient(ellipse at bottom, rgb(9,76,114) 0%, rgb(1,41,63) 100%)',
+    background: '-webkit-radial-gradient(bottom, ellipse, rgb(9,76,114) 0%, rgb(1,41,63) 100%)',
+    background: 'radial-gradient(ellipse at bottom,  rgb(9,76,114) 0%,rgb(1,41,63) 100%)',
+    filter: `progid:DXImageTransform.Microsoft.gradient( startColorstr='#094c72', endColorstr='#01293f',GradientType=1 )`
+  },
+
 };
 
 function App() {
   return (
-    <div style={divStyle}>
+    <div style={divStyle.body}>
+      <div class="stars small"></div>
+      <div class="stars medium"></div>
+      <div class="stars large"></div>
       <Animated />
     </div>
   );
